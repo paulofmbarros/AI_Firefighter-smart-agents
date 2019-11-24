89369d45bdc703a6e6abff2ac22b496980679fee
Paulo Barros <paulofmb@hotmail.com>
2019-11-24 21:58:11 +0000

    Remove fire started

--- Ai_Firefighters/src/Agents/WorldAgent.java
+++ Ai_Firefighters/src/Agents/WorldAgent.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import Agents.FireStarterAgent.StartAFire;
+import Agents.FirestationAgent.CountNumberOfVehicles;
+import Agents.FirestationAgent.ReceiveMessages;
 import Classes.ExtinguishedFire;
 import Classes.Fire;
 import Classes.WaterResource;
@@ -15,6 +17,7 @@ import World.WorldObject;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
 import java.util.Map;
@@ -24,9 +27,12 @@ import java.awt.Point;
 public class WorldAgent extends Agent{
 	
 	protected void setup() {
-		addBehaviour( new StartFireInWorld(this));
+		SequentialBehaviour sb = new SequentialBehaviour();
+		sb.addSubBehaviour(new StartFireInWorld(this));
+		sb.addSubBehaviour(new FinishFireInWorld(this));
+		addBehaviour(sb);
 	}
-class StartFireInWorld extends CyclicBehaviour {
+	class StartFireInWorld extends CyclicBehaviour {
 		
 		/**
 		 * 
@@ -61,7 +67,6 @@ class StartFireInWorld extends CyclicBehaviour {
 						content = msg.getContentObject();
 						
 						if(content instanceof FireMessage) {
-							System.out.println("Recebida a mensagem no World Agent");
 							FireMessage fm = (FireMessage) content;
 							WorldObject fireWorldObject = new WorldObject(WorldObjectEnum.FIRE, new Point(fm.getFireCoordX(), fm.getFireCoordY()));
 						    
@@ -91,6 +96,75 @@ class StartFireInWorld extends CyclicBehaviour {
 			   	
 		}
 }
+	
+	
+	class FinishFireInWorld extends CyclicBehaviour{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public FinishFireInWorld(WorldAgent worldAgent) {
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void action() {
+			ACLMessage msg = receive();
+			if(msg == null) {
+				block();
+				return;
+			}
+			
+			
+				try {
+					Object content = msg.getContentObject();
+				} catch (UnreadableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if (msg.getPerformative()==ACLMessage.INFORM) {
+					Object content;
+					try {
+						content = msg.getContentObject();
+						
+						if(content instanceof FireMessage) {
+							FireMessage fm = (FireMessage) content;
+							//get the world fire object
+							
+							if(getNumCurrentlyActiveFires()>0) {
+								
+							}
+							else {
+								//significa que todos os fogos já estão extintos
+							}
+							WorldObject fireWorldObject = new WorldObject(WorldObjectEnum.FIRE, new Point(fm.getFireCoordX(), fm.getFireCoordY()));
+						    
+						   	Map<Integer, Fire> fires = WorldAgent.getCurrentlyActiveFires();
+						   	Fire fire=null;
+						   	if(fires!=null) {
+						   		fire = new Fire((fires.size() + 1), fireWorldObject);
+						   	}
+						   	else {
+						   		fire = new Fire((1), fireWorldObject);
+						   	}
+						   	// Add, effectively, the Fire to the World
+						   	addFire(fm.getFireCoordX(), fm.getFireCoordY(), fire);
+//							
+						}
+
+					} catch (UnreadableException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+										
+					
+					
+				}
+		}
+		
+	}
 	/**
 	 * The current type of the Weather Season from the set {Spring, Summer, Autumn and Winter}
 	 */
@@ -322,6 +396,40 @@ class StartFireInWorld extends CyclicBehaviour {
 		currentlyActiveFires.put(fire.getID(), fire);
 		
 		System.out.println("Fogo adicionado a lista de fogos ativos no mapa");
+	}
+	
+	/**
+	 * Removes a Fire from a given position/point in the World's map/grid.
+	 * 
+	 * @param firePositionX coordinate X of the position/point of the World's map/grid
+	 * @param firePositionY coordinate Y of the position/point of the World's map/grid
+	 * @param idVehicleResponsibleForExtinguishFire the ID of the Vehicle Agent
+	 *        responsible for extinguish the Fire
+	 */
+	public void removeFire(int firePositionX, int firePositionY, int idVehicleResponsibleForExtinguishFire) {
+		
+		// The position of the pretended Fire to be removed can't be null
+		if(this.worldMap[firePositionX][firePositionY] != null) {
+			
+			// The position of the pretended Fire to be removed must be, as obvious, an instance of a Fire
+			if(this.worldMap[firePositionX][firePositionY] instanceof Fire) {
+				
+				Fire fireToBeExtinguished = (Fire) this.worldMap[firePositionX][firePositionY];
+				
+				// Remove the Fire to the World's map/grid, changing its position/point to null
+				this.worldMap[firePositionX][firePositionY] = null;
+				
+				// Remove the Fire from the Currently Active Fires
+				this.currentlyActiveFires.remove(fireToBeExtinguished.getID());
+				
+				ExtinguishedFire extinguishedFire = new ExtinguishedFire(fireToBeExtinguished, idVehicleResponsibleForExtinguishFire);
+				
+				// Add the Fire to the Extinguished Fires
+				this.extinguishedFires.add(extinguishedFire);
+			}
+		}
+		
+		System.out.println("Fogo removido da lista de fogos ativos no mapa");
 	}
 	}
 
